8006371213bca52e8a4170a7bdf80e63df86d801
Ade2233 <78358142+Ade2233@users.noreply.github.com>
2021-04-05 18:08:27 +0100

    Player Movement
    Not working

---
+++ City Cleanup Project/Assets/Scripts/PlayerMovementScript.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    public float forceStrength;
+    // Start is called before the first frame update
+    void Start()
+    {
+        public void MoveUp()
+        {
+            Rigidbody2D ourRigidBody = GetComponent<Rigidbody2D>();
+            PlayerObject.AddForce(Vector2.up * forceStrength);
+        }
+        public void MoveDown()
+        {
+            Rigidbody2D ourRigidBody = GetComponent<Rigidbody2D>();
+            PlayerObject.AddForce(Vector2.down * forceStrength);
+        }
+        public void MoveLeft()
+        {
+            Rigidbody2D ourRigidBody = GetComponent<Rigidbody2D>();
+            PlayerObject.AddForce(Vector2.left * forceStrength);
+        }
+        public void MoveRight()
+        {
+            Rigidbody2D ourRigidBody = GetComponent<Rigidbody2D>();
+            PlayerObject.AddForce(Vector2.right * forceStrength);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ City Cleanup Project/Assets/Scripts/ourRigidBody.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ourRigidBody : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Rigidbody2D ourRigidBody;
+        ourRigidBody = GetComponent<Rigidbody2D>();
+        Vector2 direction = Vector.right;
+        float strength = 10;
+        ourRigidBody.AddForce(direction.normalized * strength);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
